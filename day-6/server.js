36b5300c300529b70091213176599dd94bbac14b
Ritik <114500316+RitikWeb22@users.noreply.github.com>
2026-01-31 01:04:07 +0530

    Change MongoDB connection string
    Updated MongoDB connection string to use 'dbrul'.

--- day-6/server.js
+++ day-6/server.js
@@ -2,7 +2,7 @@ const app = require("./src/app");
 const MongoDB = require("mongoose");
 
 function MongodbConnect() {
-    MongoDB.connect("mongodb+srv://ritikweb30_db_user:idEjTU8KFdhqnaT2@cluster0.bpriw9o.mongodb.net/day-6").then(() => {
+    MongoDB.connect("dbrul).then(() => {
         console.log("Database is connected");
     }).catch((err) => {
         console.log(err);
